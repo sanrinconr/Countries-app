c57436528ce43ee7053d7ea59e470c18303fe47a
Santiago Rincon <santiagorincon2001@gmail.com>
2021-03-16 01:19:08 -0500

    Correccion error modelo

--- api/src/sequelize/relaciones.js
+++ api/src/sequelize/relaciones.js
@@ -2,7 +2,7 @@
 function generarRelaciones(sequelize) {
 	const { Actividad, Pais } = sequelize.models;
 
-	Actividad.belongsToMany(Pais , {foreignKey:"Actividad_id", through: "Actividad__Pais"});
+	Actividad.belongsToMany(Pais , {foreignKey:"Actividad_id", through: "Actividad_Pais"});
 	Pais.belongsToMany(Actividad , {foreignKey:"Pais_id", through: "Actividad_Pais"});
     sequelize.sync({force:true})
 }
