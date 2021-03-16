3af131c2b0d5d4575b4c2cf5147f91b9a4ba0367
Santiago Rincon <santiagorincon2001@gmail.com>
2021-03-16 00:49:25 -0500

    Creada estructura

--- api/src/routes/activity/activity.js
+++ api/src/routes/activity/activity.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
-
+const {crearActividad} = require("./db")
 const router = Router();
 
 router.post("/", function(req,res){
-    res.send(req.body)
+    const {nombre, dificultad,duracion,temporada} = req.body
+    crearActividad(nombre,dificultad,duracion,temporada)
+    .then(resp=>res.send(resp))
 })
+
 
 module.exports = router;
---
+++ api/src/routes/activity/db.js
@@ -0,0 +1,7 @@
+function crearActividad(nombre,dificultad,duracion,temporada){
+    return new Promise(function(res,rej){
+        res("HOlis")
+    })
+}
+
+module.exports = {crearActividad}
