cfcb5c418abc8860c83361a58c0762b42920e5b9
Santiago Rincon <santiagorincon2001@gmail.com>
2021-03-16 01:19:28 -0500

    Se garantiza que el pais exista en la db

--- api/src/routes/activity/activity.js
+++ api/src/routes/activity/activity.js
@@ -3,8 +3,8 @@ const {crearActividad} = require("./db")
 const router = Router();
 
 router.post("/", function(req,res){
-    const {nombre, dificultad,duracion,temporada} = req.body
-    crearActividad(nombre,dificultad,duracion,temporada)
+    const {nombre, dificultad,duracion,temporada, paises} = req.body
+    crearActividad(nombre,dificultad,duracion,temporada,paises)
     .then(resp=>{
         res.send(resp)
     })
--- api/src/routes/activity/db.js
+++ api/src/routes/activity/db.js
@@ -1,17 +1,34 @@
 const {models} = require('../../sequelize/db');
-
-function crearActividad(nombre,dificultad,duracion,temporada){
+const {buscarPorId} = require("../countries/db")
+function crearActividad(nombre,dificultad,duracion,temporada, paises){
     return models.Actividad.create({
         Nombre:nombre,
         Dificultad:dificultad,
         Duracion:duracion,
         Temporada:temporada,
     })
+    .then(actividad=>{
+        if(paises.length !==0){
+            return _vincularActividadConPais(actividad, paises)
+        }
+        return false
+    })
     .then(()=>{
-        return {nombre,dificultad,duracion,temporada}
+        return {nombre,dificultad,duracion,temporada, paises}
     })
     .catch(err=>{
         return {error:"Error",details:err}
+    })
+}
+function _vincularActividadConPais(objActividad, paises){
+
+    Promise.all(paises.map(pais=>buscarPorId(pais)))
+    .then(res=>{
+        console.log(res)
+        return res
+    })
+    .catch(err=>{
+        return {err:"Error",detail:err}
     })
 }
 
