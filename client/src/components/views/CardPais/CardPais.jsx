c0a3fe5b3a82811c410e094a3e804afd897e034a
Santiago Rincon <santiagorincon2001@gmail.com>
2021-03-20 17:10:25 -0500

    Primeros estilos para las cards

--- client/src/components/views/CardPais/CardPais.jsx
+++ client/src/components/views/CardPais/CardPais.jsx
@@ -1,17 +1,10 @@
 import "./CardPais.scss"
+import prueba from "./img/ala.svg"
 export default function CardPais({id, nombre, continente, bandera}){
     return <div className="cardPais">
-        <div>
-         <span>Id: {id}</span>
-        </div>
-        <div>
-            <span>Nombre: {nombre}</span>
-        </div>
-        <div>
-            <span>Continenete: {continente}</span>
-        </div>
-        <div>
-            <span>Bandera: {bandera}</span>
-        </div>
-    </div>
+         <span  className="nombreCard">{nombre} ({id})</span>
+         <img className="banderaCard" alt="" src={prueba}/>
+         <span className="continenteCard">Continenete: {continente}</span>
+
+</div>
 }
