cd8487d656dfb857f4527be8531224d29bd3d828
Santiago Rincon <santiagorincon2001@gmail.com>
2021-03-20 17:30:52 -0500

    Se agrega radius a las cards

--- client/src/components/views/CardPais/CardPais.jsx
+++ client/src/components/views/CardPais/CardPais.jsx
@@ -1,10 +1,15 @@
+import { useEffect, useState } from "react"
 import "./CardPais.scss"
 import prueba from "./img/ala.svg"
+import axios from "axios"
 export default function CardPais({id, nombre, continente, bandera}){
-    return <div className="cardPais">
-         <span  className="nombreCard">{nombre} ({id})</span>
-         <img className="banderaCard" alt="" src={prueba}/>
-         <span className="continenteCard">Continenete: {continente}</span>
+   
 
-</div>
+        return <div className="cardPais">
+        <img className="banderaCard" alt="" src={bandera}/>
+        <span  className="nombreCard">{nombre} ({id})</span>
+        <span className="continenteCard">Continenete: {continente}</span>
+        </div>
+
+    
 }
