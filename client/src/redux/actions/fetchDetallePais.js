a69e1b656a4656ca3cb3197ffaaee1a0e11433b6
Santiago Rincon <santiagorincon2001@gmail.com>
2021-03-22 15:38:27 -0500

    Se crea reducer para detalle

---
+++ client/src/redux/actions/fetchDetallePais.js
@@ -0,0 +1,39 @@
+import axios from "axios"; 
+import env from "react-dotenv";
+
+export const FETCH_DETALLE_PAIS_REQUEST = "FETCH_DETALLE_PAIS_REQUEST"
+export const FETCH_DETALLE_PAIS_SUCCESS = "FETCH_DETALLE_PAIS_SUCCESS"
+export const FETCH_DETALLE_PAIS_ERROR = "FETCH_DETALLE_PAIS_ERROR"
+
+
+function _fetchDetallePaisRequest(){
+    return {
+        type:FETCH_DETALLE_PAIS_REQUEST,
+    }
+}
+function _fetchDetallePaisSuccess(paises){
+    return {
+        type:FETCH_DETALLE_PAIS_SUCCESS,
+        payload:paises
+    }
+}
+function _fetchDetallePaisError(err){
+    return {
+        type:FETCH_DETALLE_PAIS_ERROR,
+        payload:err
+    }
+}
+
+export default function fetchDetallePais(id){
+    //El dispach se recibe gracias a thunk
+    return (dispatch) =>{
+        dispatch(_fetchDetallePaisRequest())
+        axios.get(env.URL_API+`countries/${id}`)
+        .then(res=>{
+            dispatch(_fetchDetallePaisSuccess(res.data))
+        })
+        .catch(err=>{
+            dispatch(_fetchDetallePaisError(err))
+        })
+    }
+}
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,13 @@
-import {applyMiddleware, createStore} from "redux"
+import {applyMiddleware, combineReducers, createStore} from "redux"
 import {composeWithDevTools} from "redux-devtools-extension"
 import thunk from "redux-thunk"
 import paisesReducer from "./reducers/paises";
-const store = createStore(paisesReducer,composeWithDevTools(
+import detallePaisReducer from "./reducers/detallePais"
+const rootReducer = combineReducers({
+    paisesReducer,
+    detallePaisReducer
+})
+const store = createStore(rootReducer,composeWithDevTools(
     applyMiddleware(thunk)
 ))
 
