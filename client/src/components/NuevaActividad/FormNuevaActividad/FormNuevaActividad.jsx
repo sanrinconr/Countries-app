52f72e724e090d4ee261c17f6dfb346079ef7b9a
Santiago Rincon <santiagorincon2001@gmail.com>
2021-03-23 18:04:44 -0500

    Se pueden crear paises dinamicamente

--- client/src/components/NuevaActividad/FormNuevaActividad/FormNuevaActividad.jsx
+++ client/src/components/NuevaActividad/FormNuevaActividad/FormNuevaActividad.jsx
@@ -1,23 +1,43 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./FormNuevaActividad.scss"
 export default function FormNuevaActividad(){
-    const [input, setInput] = useState({
+    const [cantPais, setCantPais] = useState(1)
+    const [paises, setPaises] = useState({0:{value:""}})
+    const [inputActividad, setInputActividad] = useState({
         nombre: "",
         dificultad: "",
         duracion:"",
         temporada:"",
-        paises:[]
       })
-      const [errors, setErrors] = useState({})
-      function handleChange(event){
-        setInput({
-          ...input,
+      const [errors, setErrors] = useState()
+      function handleChangeActividad(event){
+        setInputActividad({
+          ...inputActividad,
           [event.target.name]: event.target.value
         })
         // setErrors(validate({
         //   ...input,
         //   [event.target.name]: event.target.value
         // }))
+      }
+      function handleChangePais(event){
+        //https://stackoverflow.com/questions/29537299/react-how-to-update-state-item1-in-state-using-setstate
+        const {id} = event.target
+        let original = {...paises}
+        let elemento = {...paises[id]}
+        elemento.value = event.target.value
+        original[id] = elemento
+        setPaises(original)
+      }
+      function nuevoSpanPais(e){
+        e.preventDefault()
+        let original = {...paises}
+        let elemento = {...paises[cantPais]}
+        elemento.value = ""
+        original[cantPais] = elemento
+        console.log(original)
+        setPaises(original)
+        setCantPais(cantPais+1)
       }
       return (
           <div className="formNuevaActividad" >
@@ -32,16 +52,16 @@ export default function FormNuevaActividad(){
                             //   className={errors.username ? 'danger':''} 
                             name="nombre" 
                             placeholder="Nombre"
-                            value={input.nombre}
-                            onChange={handleChange}/>
+                            value={inputActividad.nombre}
+                            onChange={handleChangeActividad}/>
                         </div>
                         <div>
                             <input type="text"
                             //   className={errors.username ? 'danger':''} 
                             name="temporada" 
                             placeholder="Temporada"
-                            value={input.temporada}
-                            onChange={handleChange}/>
+                            value={inputActividad.temporada}
+                            onChange={handleChangeActividad}/>
                         </div>
                         <div>
                             <input type="submit" 
@@ -51,34 +71,37 @@ export default function FormNuevaActividad(){
                     <div className="derecha">
                         <div>
                             <input type="text"
-                            className={errors.password ? 'danger':''} 
+                            // className={errors.password ? 'danger':''} 
                             name="dificultad"
                             placeholder="Dificultad"
-                            value={input.dificultad}
-                            onChange={handleChange}/>
+                            value={inputActividad.dificultad}
+                            onChange={handleChangeActividad}/>
                         </div>
                         <div>
                             <input type="text"
-                            className={errors.password ? 'danger':''} 
+                            // className={errors.password ? 'danger':''} 
                             name="duracion"
                             placeholder="Duracion"
-                            value={input.duracion}
-                            onChange={handleChange}/>
+                            value={inputActividad.duracion}
+                            onChange={handleChangeActividad}/>
                         </div>
                     </div>
                 </div>
                 <div className="paises">
                     <span>Paises</span>
-                    <div>
-                            <input type="text"
-                            className={errors.password ? 'danger':''} 
-                            name="nombrePais"
-                            placeholder="Pais"
-                            value={input.nombrePais}
-                            onChange={handleChange}/>
-                    </div>
-                    
-                    <div > <button className="nuevoPais">+</button></div>
+                    {Object.keys(paises).map(claveId=>{
+                        return <div key={claveId} >
+                             <input type="text"
+                             id={claveId}
+                             key={claveId}
+                            //  className={errors.password ? 'danger':''} 
+                             name="pais"
+                             placeholder="Pais"
+                             value={paises[claveId].value}
+                             onChange={handleChangePais}/>
+                         </div>
+                    })}
+                    <div > <button className="nuevoPais" onClick={nuevoSpanPais}>+</button></div>
                 </div>
 
           </form>
