540c44fb980b9c93b3a17e6c71d0da09a791066e
Santiago Rincon <santiagorincon2001@gmail.com>
2021-03-24 12:59:37 -0500

    Filtro alfabetico funciona

--- client/src/components/Home/Filtros/Filtros.jsx
+++ client/src/components/Home/Filtros/Filtros.jsx
@@ -1,21 +1,67 @@
-export default function Filtros(){
+import { useState } from "react"
+import {connect} from "react-redux"
+
+import fetchPaises from "../../../redux/actions/fetchPaises"
+
+function Filtros({cambiarOrden, paginaSiguiente, orden}){
+    const [input , setInput] = useState({
+        continente:"",
+        actividad:"",
+    })
+    function handleChange(e){
+        e.preventDefault()
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        })
+        console.log(input)
+    }
+    function handleChangeAlfabeto(e){
+        if(orden !== e.target.value){
+            cambiarOrden(0, e.target.value)
+        }
+        e.preventDefault()
+
+    }
     return <div className="filtros">
         <div className="filtroAlfabetico">
-            <button>Ascendentemente</button>
-            <button>Descendentemente</button>
+            <button name="nombre" value="ASC"  onClick={handleChangeAlfabeto}>Ascendentemente</button>
+            <button name="nombre" value="DESC"  onClick={handleChangeAlfabeto}>Descendentemente</button>
         </div>
         <div className="filtroContinente">
-            <select className='select' name="temporada">
-                <option value="">America del sur</option>
-                <option value="">America del norte</option>
-                <option value="">Europa</option>
-                <option value="">Antartida</option>
+            <select className='select' name="continente"  onChange={handleChange}>
+                <option value="Asia">Asia</option>
+                <option value="Africa">Africa</option>
+                <option value="Americas">America</option>
+                <option value="Polar">Antartida</option>
+                <option value="Oceania">Oceania</option>
+
             </select>
         </div>
-        <div className="filtroActividades">
-        <select className='select' name="temporada">
+        {/* <div className="filtroActividades">
+        <select className='select' name="actividad" onChange={handleChange}> 
                 <option value="test1">test1</option>
+                <option value="test2">test2</option>
+                <option value="test3">test3</option>
+
             </select>
-        </div>
+        </div> */}
     </div>
 }
+
+//Para conocer que paises existen actualmente
+const mapStateToProps = (state)=>{
+    return state.paisesReducer
+};
+//Para poder pedir los primeros 10 paises
+const mapDispatchToProps = (dispatch, ownProps) => ({
+    cambiarOrden: (paginaSiguiente, orden) => {
+            dispatch(fetchPaises(paginaSiguiente, orden))
+    }
+})
+
+
+export default connect(
+mapStateToProps,
+mapDispatchToProps
+)(Filtros)
