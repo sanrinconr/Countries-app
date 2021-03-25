8b6d686ded65f0590ef32a25a97a92b3783caf98
Santiago Rincon <santiagorincon2001@gmail.com>
2021-03-25 16:06:36 -0500

    El home carga los paises iniciales

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -7,10 +7,29 @@ import SearchBar from "./SearchBar/SearchBar";
 import ContainerPaises from "./ContainerPaises/ContainerPaises";
 import "./Home.scss"
 import Filtros from "./Filtros/Filtros";
-export default function Home(){
+import fetchPaises from "../../redux/actions/fetchPaises";
+import { connect } from "react-redux";
+import { useEffect } from "react";
+function Home({cargarPaisesInicial}){
+    useEffect(()=>{
+        cargarPaisesInicial()
+    },[])
     return <div className="home">
         <Filtros/>
         <SearchBar/>
         <ContainerPaises/>
     </div>
 }
+
+//Se cargaran los primeros 10 en redux
+const mapDispatchToProps = (dispatch, ownProps) => ({
+    cargarPaisesInicial: () => {
+            dispatch(fetchPaises())
+    }
+})
+
+
+export default connect(
+null,
+mapDispatchToProps
+)(Home)
