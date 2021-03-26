85b9f298a43f4b6a50bb058d4e1cbe36d8102d23
Santiago Rincon <santiagorincon2001@gmail.com>
2021-03-26 09:58:20 -0500

    Finalizados test countries

--- api/tests/countries.spec.js
+++ api/tests/countries.spec.js
@@ -6,7 +6,7 @@ describe('Pagina home', () => {
 	it('Pagina de bienvenida api', async (done) => {
 		const response = await request(app).get('/')
 		expect(response.statusCode).toBe(200)
-    expect(response.body.mensaje).toBe("Bienvenido a la api de paises")
+		expect(response.body.mensaje).toBe("Bienvenido a la api de paises")
 		done()
 	})
 })
@@ -15,29 +15,38 @@ describe('Pagina countries', () => {
 	it('Se obtinen los primeros 10 paises', async (done) => {
 		const response = await request(app).get('/countries')
 		expect(response.statusCode).toBe(200)
-    expect(response.body.length).toBe(10)
+		expect(response.body.length).toBe(10)
 		done()
 	})
-  it('El primer pais debe ser AFG', async (done) => {
+	it('El primer pais debe ser AFG', async (done) => {
 		const response = await request(app).get('/countries')
-    expect(response.body[0].Id).toBe("AFG")
+		expect(response.body[0].Id).toBe("AFG")
 		done()
 	})
-  it('Paginado, primer pais de la pagina 1', async (done) => {
+	it('Paginado, primer pais de la pagina 1', async (done) => {
 		const response = await request(app).get('/countries?page=1')
-    expect(response.body[0].Id).toBe("ARG")
+		expect(response.body[0].Id).toBe("ARG")
 		done()
 	})
 })
 
-describe("Filtros", ()=>{
-  it("Filtrado de A-Z", async(done)=>{
-    const response = await request(app).get('/countries?page=0  ')
+describe("Filtros", () => {
+	it("Filtrado de A-Z", async (done) => {
+		const response = await request(app).get('/countries?page=0&orden=ASC  ')
 		expect(response.statusCode).toBe(200)
-    expect(response.body.length).toBe(10)
+		expect(response.body[0].Id).toBe("AFG")
 		done()
-  })
+	})
+	it("Filtrado Z-A", async (done) => {
+		const response = await request(app).get('/countries?page=0&orden=DESC  ')
+		expect(response.statusCode).toBe(200)
+		expect(response.body[0].Id).toBe("ZWE")
+		done()
+	})
 })
+
+
+
 
 afterAll(() => async (done) => {
 	// Closing the DB connection allows Jest to exit successfully.
